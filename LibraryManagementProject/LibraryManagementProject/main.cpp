d7919150d093e0abe5bdad939c0d239f6f5e368f
Konrad-Z - N1179676 <konrad.zajac2024@my.ntu.ac.uk>
2025-04-18 12:16:43 +0100

    Comments

--- LibraryManagementProject/LibraryManagementProject/main.cpp
+++ LibraryManagementProject/LibraryManagementProject/main.cpp
@@ -1,23 +1,25 @@
+// Library Management System by Konrad Zajac (N1179676) 2025
+
+// Libraries
 #include <stdlib.h>
 #include <iostream>
-
 #include "mysql_connection.h"
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/prepared_statement.h>
 #include <cppconn/statement.h>
 #include <cppconn/resultset.h>
-
 using namespace std;
 
-string GetDatabasePassword() {
+// Function to get the database password from the user, the password isnt just hardcoded
+string GetDatabasePassword() { 
     string Password;
     cout << "Enter MySQL password: ";
     cin >> Password;  // For actual security, replace with a method that hides input
     return Password;
 }
 
-
+// Database constants
 const string server = "tcp://127.0.0.1:3306";
 const string username = "root";
 const string password = GetDatabasePassword();
@@ -33,23 +35,15 @@ int main()
 
     try // Try catch statement  if the system cannot connect to the server
     {
-        // Get MySQL driver instance
+        // Get MySQL driver instance (Gets the specific MySQL program that is running to manage the database)
         driver = get_driver_instance();
-
-        // Establish connection
+        // Logs into the database using user credentials
         con = driver->connect(server, username, password);
-
-        // Set the schema (replace with your actual schema name)
+        //Sets which database to use, The system can store multiple, I only require 1
         con->setSchema("librarysystem");
-
-        
-    
-        // Clean up resources
-        delete res;
-        delete stmt;
-        delete con;
+ 
     }
-    catch (sql::SQLException& e)
+    catch (sql::SQLException& e) // Error message if the system could not connect to the database
     {
         cout << "Could not connect to server. Error message: " << e.what() << endl;
         system("pause");
@@ -72,3 +66,8 @@ int main()
         }
 
 */
+
+// Clean up resources
+        //delete res;
+       //delete stmt;
+        //delete con;
